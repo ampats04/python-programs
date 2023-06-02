69bdb53eae0feb102e07d134c6a7be2742d7ee58
ampats04 <jeremyandyampatin@gmail.com>
2023-06-02 20:46:50 +0800

    Display func added in Linkedlist

--- linked_list.py
+++ linked_list.py
@@ -3,28 +3,43 @@ class Node:
         self.data = data
         self.next = None
 
-class LinkedList:
 
+class LinkedList:
     def __init__(self):
         self.head = None
-    
-    def append(self,data):
+
+    def append(self, data):
         new_node = Node(data)
 
         if self.head is None:
             self.head = new_node
-        
+
         else:
             current = self.head
-            
+
             while current.next:
                 current = current.next
             current.next = new_node
-    
-    
-    def main():
+
+    def display(self):
+        current = self.head
+
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+
+        print("None")
+
+
+def main():
+    linedlist = LinkedList()
+
+    linedlist.append(10)
+    linedlist.append(20)
+    linedlist.append(30)
+
+    linedlist.display()
 
 
 if __name__ == "__main__":
     main()
-
