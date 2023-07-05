eae5155f3407cc80b700e3fdb379fc52aa5c4271
ampats04 <jeremyandyampatin@gmail.com>
2023-07-05 23:00:48 +0800

    [Example]: Tribonnaci Sequence

--- Tribonnaci.py
+++ Tribonnaci.py
@@ -1,8 +1,22 @@
 class Tribonnaci:
     def tribonnaci(signature, n):
-        return n
+        if n == 0:
+            return []
+        elif n <= 3:
+            return signature[:n]
+
+        result = signature[:]
+
+        for i in range(3, n):
+            next_number = sum(result[-3])
+            result.append(next_number)
 
 
 def main():
-    if __name__ == "__main__":
-        main()
+    tr = Tribonnaci
+
+    tr.tribonnaci([1, 1, 1], 10)
+
+
+if __name__ == "__main__":
+    main()
